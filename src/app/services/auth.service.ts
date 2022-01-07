3257ea0fceb14e17674736197fb96415d48dc5e7
Victoire Nyoung <nyoungvictoire@gmail.com>
2022-01-07 14:26:34 +0100

    probleme de then catch

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,58 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private api = environment.api;
+  token: string;
+  userId: string;
+  isAuth$ = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.api+'/users/signup', {email: email, password: password}).subscribe((signupData: {status: number, message: string } )=>{
+        if (signupData.status === 201) {
+          // authentifier l'utilisateur
+          this.signin(email, password);
+          .then(() => {
+            resolve(true)
+          }).catch((err) => {
+              reject(err)
+          });
+
+        }else {
+          reject(signupData.message);
+        }
+      },
+      (err)=>{
+        reject(err)
+      })
+    })
+  }
+
+  signin(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.api+'/users/login', {email: email, password: password}).subscribe((authData: {token: string, userId: string})=>{
+            this.token = authData.token;
+            this.userId = authData.userId;
+            this.isAuth$.next(true);
+            resolve(true);
+          },
+          (err)=>{
+            reject(err)
+          }
+        )
+    })
+  }
+
+  logout() {
+
+  }
 }
